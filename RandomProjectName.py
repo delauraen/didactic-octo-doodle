de4878c88937d568dc2959359ffe5e6edec662f4
delauraen <22270079+delauraen@users.noreply.github.com>
2024-02-27 20:18:28 -0800

    main now generates 5 diff project names at once, and runs initially

--- RandomProjectName.py
+++ RandomProjectName.py
@@ -4,19 +4,20 @@ from wonderwords import RandomWord
     
 r = RandomWord()
 
-# TODO generate 5 project names at a time instead of 1
-
 def main():
     while True:
+
+        for i in range(5):
+            firstWord = r.word(include_parts_of_speech=["adjectives"])
+            secondWord = r.word(include_parts_of_speech=["nouns"])
+            print(firstWord, secondWord)
+
         action = input("Generate another (enter) or quit (q)").strip()
 
         if action.lower() == "q":
             break
-        firstWord = r.word(include_parts_of_speech=["adjectives"])
-        secondWord = r.word(include_parts_of_speech=["nouns"])
-        print(firstWord, secondWord)
-    print("Goodbye!")
 
+    print("Goodbye!")
 
 if __name__ == "__main__":
     main()
